205d0b6ca3461ebb4950665b8e0a9128ec6ec136
p-karol <priscilla.karolczak@gmail.com>
2020-04-01 03:24:24 +0100

    pom.xml corrected

--- src/main/java/com/csv-motivator/PersistenciaVeiculos.java
+++ src/main/java/com/csv-motivator/PersistenciaVeiculos.java
@@ -17,7 +17,7 @@ import static java.nio.file.StandardOpenOption.APPEND;
 
 public class PersistenciaVeiculos{
     
-    private static final String sample_csv_file_path = "./veiculos.dat";
+    private static final String sample_csv_file_path = "veiculos.dat";
   
     public List<Veiculo> carregaVeiculos() throws IOException{
         
@@ -34,8 +34,8 @@ public class PersistenciaVeiculos{
                     String Marca = csvRecord.get(1);
                     String Cor = csvRecord.get(2);
                     String Categoria = csvRecord.get(3);
-                   // Veiculo v = new Veiculo(Placa, Marca, Cor, Categoria);
-                   // lista.add(v);
+                    Veiculo v = new Veiculo(Placa, Marca, Cor, Categoria);
+                    lista.add(v);
             }
         return lista;
         } 
